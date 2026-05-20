9036a13422658a3733cee92a69b9dc3331d1e72e
EpicEditEmporium652 <parklink789@gmail.com>
2026-05-20 23:34:42 +0100

    Update serverviewer_server.lua

--- Scripts/Showcase/serverviewer_server.lua
+++ Scripts/Showcase/serverviewer_server.lua
@@ -3,6 +3,7 @@ streamEvent.Name = "RemoteEvent"
 local toggleEvent = Instance.new("RemoteEvent", game.ReplicatedStorage)
 toggleEvent.Name = "tgl"
 local connections = {}
+local savedPosition = {}
 
 toggleEvent.OnServerEvent:Connect(function(player)
     if connections[player] then
@@ -13,7 +14,7 @@ toggleEvent.OnServerEvent:Connect(function(player)
     connections[player] = game:GetService("RunService").Heartbeat:Connect(function()
         local cframeData = {}
         for _, v in workspace:GetDescendants() do
-            if v:IsA("BasePart") and not v:IsA("SpawnLocation") and v.Name ~= "Baseplate" then
+            if v:IsA("BasePart") and not v:IsA("SpawnLocation") and v.Name ~= "Baseplate" and (not next(savedPosition) or savedPosition[v:GetFullName()] ~= v.CFrame) then
                 local parentModel = v:FindFirstAncestorOfClass("Model")
                 if parentModel and parentModel:FindFirstChild("Humanoid") and parentModel.Name ~= player.Name then
                     continue
